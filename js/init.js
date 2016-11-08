241d6a88225805e376aa02bb9f2a11a4e236d7c6
nightbox69 <cglmelo@gmail.com>
2016-11-08 17:52:06 +0800

    React-Carousel HMTL Sample flickity-metafizzy

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
-$(".slider-autoplay").slick({
-  dots: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.carousel', {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  freeScroll: true,
+  autoPlay: true,
+  wrapAround: true
 });
