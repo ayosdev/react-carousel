6c3e6642fc119c612f33d14c39b5c1391980d85f
nightbox69 <cglmelo@gmail.com>
2016-11-10 17:55:39 +0800

    React-Carousel Plugin

---
+++ plugin/lib/index.js
@@ -0,0 +1,56 @@
+var React = require('react')
+var Flick = require('flickity')
+var refName = 'carousel'
+
+var Flickity = React.createClass ({
+
+  propTypes: {
+    options: React.PropTypes.object,
+    className: React.PropTypes.string,
+    elementType: React.PropTypes.string,
+    children: React.PropTypes.array
+  },
+
+  getInitialState: function () {
+    return {
+      selectedIndex: 0
+    }
+  },
+
+  componentDidMount: function() {
+    var carousel = this.refs.carousel.getDOMNode();
+    this.flkty = new Flickity(carousel, this.props.options);
+    this.flkty.on('cellSelect', this.updateSelected);
+  }
+
+  getDefaultProps: function () {
+    return {
+      options: {},
+      className: '',
+      elementType: 'div'
+    }
+  },
+
+  updateSelected: function () {
+    var index = this.flkty.selectedIndex
+    this.setState({
+      selectedIndex: index
+    })
+  },
+
+  componentWillUnmount: function () {
+    if (this.flkty) {
+      this.flkty.off('cellSelect', this.updateSelected)
+      this.flkty.destroy()
+    }
+  },
+
+  render: function () {
+    return React.createElement(this.props.elementType, {
+      className: this.props.className,
+      ref: refName
+    }, this.props.children)
+  }
+});
+
+module.exports = Flickity;
